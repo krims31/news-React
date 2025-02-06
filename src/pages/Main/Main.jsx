ad606d72896e95426f923f220f4cac797214e249
krims31 <vegor405@gmail.com>
2025-02-06 18:07:02 +0300

    Add files via upload

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,29 @@
+import styles from './styles.module.css'
+import { getNews } from '../../api/apiNEWS'
+import NewsBanner from '../../components/NewsBanner/NewsBanner'
+import NewsList from '../../components/NewsList/NewsList'
+import { useEffect, useState } from 'react'
+
+const Main = () => {
+    const [news, setNews] = useState([])
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await getNews();
+        setNews(response.news)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchNews();
+  }, []);
+  return (
+    <main className={styles.main}>
+      {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
+
+      <NewsList news={news} />
+    </main>
+  )
+}
+
+export default Main
