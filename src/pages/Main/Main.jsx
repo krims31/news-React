80898de08012318f5104b524d3aab05dd69786f8
krims31 <vegor405@gmail.com>
2025-02-07 09:26:01 +0300

    Add files via upload

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,14 +3,18 @@ import { getNews } from '../../api/apiNEWS'
 import NewsBanner from '../../components/NewsBanner/NewsBanner'
 import NewsList from '../../components/NewsList/NewsList'
 import { useEffect, useState } from 'react'
+import Skeleton from '../../components/Skeleton/Skeleton'
 
 const Main = () => {
-    const [news, setNews] = useState([])
+    const [news, setNews] = useState([]);
+    const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
     const fetchNews = async () => {
       try {
+        setIsLoading(true)
         const response = await getNews();
         setNews(response.news)
+        setIsLoading(false)
       } catch (error) {
         console.log(error)
       }
@@ -19,9 +23,9 @@ const Main = () => {
   }, []);
   return (
     <main className={styles.main}>
-      {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
+      {news.length > 0 && !isLoading ? <NewsBanner item={news[0]} /> : <Skeleton type={'banner'} count={1}/>}
 
-      <NewsList news={news} />
+      {!isLoading ? <NewsList news={news} /> : <Skeleton type={'item'} count={10} />}
     </main>
   )
 }
